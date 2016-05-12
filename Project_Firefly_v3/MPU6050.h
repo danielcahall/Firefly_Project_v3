56f265030249237c15ef84e3dd620d0ec294f8ff
danielcahall <cahalld0@students.rowan.edu>
2016-05-11 20:52:15 -0400

    Add files via upload

---
+++ Project_Firefly_v3/MPU6050.h
@@ -0,0 +1,24 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: Dec 21, 2015
+ *      Author: Danny
+ */
+
+#ifndef MPU6050_H_
+#define MPU6050_H_
+
+extern unsigned char RX_ByteCtr;
+extern unsigned char TX_ByteCtr;
+
+void initMPU6050();
+void getMPU6050();
+int getXAccel();
+int getYAccel();
+int getZAccel();
+int getXAngle();
+int getYAngle();
+int getZAngle();
+int getTemp();
+
+#endif /* MPU6050_H_ */
